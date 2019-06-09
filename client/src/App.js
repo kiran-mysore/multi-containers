671782f8ccfa8f92989d1ae2da32c8b982a90bf6
Kiran Rao <kiran.mysore.rao@gmail.com>
2019-06-09 20:58:19 +0200

    Changed to trigger the build

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
             Learn React
           </a>
           <Link to="/">Go to Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">Other-Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
